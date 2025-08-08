c02b39fd599e35048723ef9d4472dfbae3c2eacd
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-08-08 11:25:06 -0300

    create heading jsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('ola');
-
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá mundo!
+      </Heading>
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
         corrupti culpa, harum repellendus ad deleniti quam provident eaque
