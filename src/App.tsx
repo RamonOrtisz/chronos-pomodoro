d1abce4ed03f002abe8aad62bdf64d46e6de86be
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-08-12 20:09:50 -0300

    create menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-        corrupti culpa, harum repellendus ad deleniti quam provident eaque
-        eligendi pariatur.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
