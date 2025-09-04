4ee6f8ef4db81d371eec4b0c4342202fd5ffad05
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-09-03 21:58:25 -0300

    config props 2

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput id='meuInput' labelText='task' type='text' />
           </div>
 
           <div className='formRow'>
--- src/components/Defaultinput/index.tsx
+++ src/components/Defaultinput/index.tsx
@@ -1,9 +1,12 @@
-type DefaultInputProps = { id: string } & React.ComponentProps<'input'>;
+type DefaultInputProps = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({ id, labelText, type }: DefaultInputProps) {
   return (
     <>
-      <label htmlFor={id}>task</label>
+      {labelText && <label htmlFor={id}>{labelText}</label>}
       <input id={id} type={type} />
     </>
   );
